a76fbc0e351e6f02e3d25f56416c15f715be7598
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2023-03-19 18:11:13 +0800

    fixed the import issue

--- MultieAnalysis/cluster.py
+++ MultieAnalysis/cluster.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn import preprocessing as pp
 import scipy.cluster.hierarchy as sch
 import matplotlib.pyplot as plt
-a = np.loadtxt("Pdata11_11.txt")
+a = np.loadtxt("cluster.txt")
 b = pp.minmax_scale(a.T) # standerlise data
 d = sch.distance.pdist(b) # to cal distance between 2 obj
 dd = sch.distance.squareform(d) # transform to matrix form
