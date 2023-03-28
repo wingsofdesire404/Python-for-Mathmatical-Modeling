d0a7eb6e1331669f5cbf2c9a3789b8c4be711d0b
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2023-03-28 19:59:13 +0800

    used for loop to automaticly draw k-mean result

--- MultieAnalysis/elbow_kmean.py
+++ MultieAnalysis/elbow_kmean.py
@@ -26,12 +26,15 @@ kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_
 y_kmeans = kmeans.fit_predict(X)
 
 # 绘制聚类结果
-plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
-plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
-plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
-plt.title('K-Means Clustering')
+k = 3
+# 可视化聚类结果
+colors = ['red', 'blue', 'green', 'black', 'cyan', 'magenta'] # 适当拓展颜色以防k过大而用光颜色集
+for i in range(k):
+    plt.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s = 100, c = colors[i], label = 'Cluster {}'.format(i+1))
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of datapoints')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.legend()
 plt.show()
+
