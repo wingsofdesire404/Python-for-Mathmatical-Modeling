696e84472aa56cc3dff9c48eb96b529961f304b2
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2023-03-28 23:38:56 +0800

    fixed random test among ts

--- TimeSeries/random_test.py
+++ TimeSeries/random_test.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.stats.diagnostic import acorr_ljungbox
+from statsmodels.tsa.stattools import adfuller
 
 # 读取数据
 data = pd.read_csv('chem.csv', index_col='date', parse_dates=True)
@@ -11,11 +10,24 @@ data = pd.read_csv('chem.csv', index_col='date', parse_dates=True)
 # 对数据进行平稳化处理
 diff_data = data.diff().dropna()
 
-# 进行Ljung-Box检验
-lbvalue, pvalue = sm.stats.acorr_ljungbox(diff_data, lags=20)
+# 进行6阶ADF检验
+result_6 = adfuller(data, maxlag=6)
 
-# 输出结果
-print("Ljung-Box统计量: ", lbvalue)
-print("p值: ", pvalue)
+# 打印结果
+# p < 0.05 为非随机
+print('ADF Statistic (6 lags): %f' % result_6[0])
+print('p-value (6 lags): %f' % result_6[1])
+print('Critical Values (6 lags):')
+for key, value in result_6[4].items():
+    print('\t%s: %.3f' % (key, value))
 
+# 进行12阶ADF检验
+result_12 = adfuller(data, maxlag=12)
+
+# 打印结果
+print('ADF Statistic (12 lags): %f' % result_12[0])
+print('p-value (12 lags): %f' % result_12[1])
+print('Critical Values (12 lags):')
+for key, value in result_12[4].items():
+    print('\t%s: %.3f' % (key, value))
 
