102e5930b6dff0035c81845be7761a7c6a0a140e
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2023-03-28 23:41:23 +0800

    fixed problem in ARIMA function and added some notes

--- TimeSeries/ARIMA_fit.py
+++ TimeSeries/ARIMA_fit.py
@@ -6,10 +6,10 @@ import statsmodels.api as sm
 data = pd.read_csv('mile.csv', index_col='date', parse_dates=True)
 
 # 定义ARIMA模型
-arima_model = sm.tsa.ARIMA(data, order=(1, 0, 1)) # p, d, q = 1, 1, 1; d表示差分阶数
+arima_model = sm.tsa.ARIMA(data, order=(2, 0, 0)) # p, d, q = 1, 1, 1; d表示差分阶数
 
 # 拟合模型并估计参数
-arima_result = arima_model.fit(method='mle')
+arima_result = arima_model.fit()
 
 # 打印估计参数结果
 print(arima_result.summary())
