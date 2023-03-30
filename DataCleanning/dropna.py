d93378fc057212791fd525b9b2efa3c2e6be6edb
WingsOfDesire404 <ScandinaviaAurora@proton.me>
2023-03-30 21:42:18 +0800

    add a method for interploting missing data and rename the data file for dropna.py

--- DataCleanning/dropna.py
+++ DataCleanning/dropna.py
@@ -1,6 +1,6 @@
 #程序文件Pex4_28.py
 from pandas import read_excel
-a=read_excel("uid.xlsx",usecols=range(1,4))
+a=read_excel("user.xlsx",usecols=range(1,4))
 b1=a.dropna()  #删除所有的缺失值
 b2=a.dropna(axis=1, thresh=9)  #删除有效数据个数小于9的列
 b3=a.drop('用户B', axis=1)      #删除用户B的数据
